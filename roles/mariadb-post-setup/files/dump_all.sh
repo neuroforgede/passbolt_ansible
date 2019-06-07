3d63c23ae056626a4c04a423ea8e4d056c9c28ee
Martin Braun <martinbraun123@aol.com>
2019-06-07 15:39:19 +0200

    add license

--- roles/mariadb-post-setup/files/dump_all.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-DATE=`date '+%Y_%m_%d_%H_%M_%S'`
-
-su mysql -c "mysqldump --all-databases" > /data/ansible/mariadb_dumps/mariadb_${DATE}.sql
--- roles/mariadb-post-setup/files/start_dump.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-/usr/bin/flock -n /tmp/mariadb-dump.lck bash /data/ansible/mariadb_dump/dump_all.sh
