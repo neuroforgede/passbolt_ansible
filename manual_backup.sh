08e28d8a83bdd34bb80cd3eabec9eb831703c13f
Martin Braun <martinbraun123@aol.com>
2019-06-08 20:07:41 +0200

    improve manual_backup to put all backed up files in the same date tagged folder

--- manual_backup.sh
+++ manual_backup.sh
@@ -1,5 +1,12 @@
 #!/bin/bash
+
+old_pwd=$(pwd)
+
 DATE=`date +"%m-%d-%y"`
-ssh -t -i ssh_keys/root_rsa root@$1 mysqldump --all-databases > "mariadb-$1-$DATE.sql"
-mkdir -p all_ansible_data_backup_$DATE
-scp -i ssh_keys/root_rsa -r root@$1:/data/ansible all_ansible_data_backup_$DATE
+BASE_DIR=passbolt_backup_$1_$DATE
+mkdir -p $BASE_DIR
+
+cd $BASE_DIR
+ssh -t -i "${old_pwd}/ssh_keys/root_rsa" root@$1 mysqldump --all-databases > "mariadb-$1-$DATE.sql"
+mkdir -p all_ansible_data_backup
+scp -i "${old_pwd}/ssh_keys/root_rsa" -r root@$1:/data/ansible all_ansible_data_backup
